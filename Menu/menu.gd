4333bc63094ba42fb59484f6e608522cd8d7ed94
Modumem <156741997+Modumem@users.noreply.github.com>
2024-07-22 19:55:32 -0400

    Cung/Remove.Godot/22

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -12,4 +12,14 @@ func _process(delta):
 
 
 func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Main Map/MainMap.tscn")
+
+
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_options_button_pressed():
 	pass # Replace with function body.
