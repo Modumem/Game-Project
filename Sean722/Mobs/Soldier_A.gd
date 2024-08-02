c39556ff7614f190d594db1d873a04218f276fb3
sphughes16 <sphughes16@gmail.com>
2024-08-01 23:06:59 -0400

    Level1TowersAndBullets

--- Sean722/Mobs/Soldier_A.gd
+++ Sean722/Mobs/Soldier_A.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
+@export var speed = 100
+var Health = 10
 
-@export var speed = 1000
 func _process(delta):
-	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	get_parent().set_progress(get_parent().get_progress() + speed * delta)
 	if get_parent().get_progress_ratio() == 1:
-		queue_free()
+		queue_free()  # Remove the soldier if at the end of the path
+	if Health <= 0:
+		queue_free()  # Remove the soldier if health drops to 0
+
+func take_damage(amount: int):
+	Health -= amount
+	if Health <= 0:
+		queue_free()  # Remove the soldier if health drops to 0
+
 func _ready():
 	# Ensure this sprite appears on top of the map
 	self.z_index = 1
